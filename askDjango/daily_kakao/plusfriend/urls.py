bc3c516491552fdf814b052376a2f059c16d801d
Kim Do Ky <makingfunk0@gmail.com>
2017-12-25 16:13:16 +0900

    daily_kakao 빠진 커밋 추가(plusfriend app)

--- askDjango/daily_kakao/plusfriend/urls.py
+++ askDjango/daily_kakao/plusfriend/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 app_name = 'plusfriend'
 urlpatterns = [
+    path('keyboard', views.on_init),
+    path('friend', views.on_added),
+    path('friend/(?P<user_key>[\w-]+)$', views.on_block),
+    path('chat_toom/(?P<user_key>[\w-]+)$', views.on_leave),
+    path('message', views.on_message),
 ]
--- askDjango/daily_kakao/plusfriend/views.py
+++ askDjango/daily_kakao/plusfriend/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from .decorators import bot
 
-# Create your views here.
+@bot
+def on_init(request):
+    return {'type': 'text'}
+
+@bot
+def on_message(request):
+    user_key = request.JSON['user_key']
+    type = request.JSON['type']
+    content = request.JSON['content']
+    response = '아직 구현 전입니다.'
+    return {
+        'message': {
+            'text': response,
+        }
+    }
+
+@bot
+def on_added(request):
+    user_key = request.JSON['user_key']
+
+@bot
+def on_block(request, user_key):
+    pass
+
+@bot
+def on_leave(request, user_key):
+    pass
