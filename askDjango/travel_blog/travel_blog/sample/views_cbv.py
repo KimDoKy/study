90eefa11dfb4903f3a8cbd230e502d17e87af9e6
Kim Do Ky <makingfunk0@gmail.com>
2017-12-28 16:54:34 +0900

    travel_blog add view_sample CBV_HttpResponse 응용

--- askDjango/travel_blog/travel_blog/sample/views_cbv.py
+++ askDjango/travel_blog/travel_blog/sample/views_cbv.py
@@ -5,11 +5,14 @@ from django.views.generic import View
 class PostListView1(View):
     def get(self, request):
         type = 'Class View HttpResponse Type'
-        html = '''
+        html = self.get_template_string().format(type=type)
+        return HttpResponse(html)
+
+    def get_template_string(self):
+        return '''
             <h1> CBV sample Page </h1>
             <p> {type} </p>
-            <p>CBV HttpResponse Sample Page입니다.</p>'''.format(type=type)
-        return HttpResponse(html)
+            <p>CBV HttpResponse Sample Page입니다.</p>'''
 
 post_list1 = PostListView1.as_view()
 
