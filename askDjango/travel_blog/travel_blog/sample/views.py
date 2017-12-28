a4516e3f225e6dde4707ede5f9153947f98faf2d
Kim Do Ky <makingfunk0@gmail.com>
2017-12-28 16:02:35 +0900

    travel_blog create testapp, add app in settings, HttpResponse

---
+++ askDjango/travel_blog/travel_blog/sample/views.py
@@ -0,0 +1,12 @@
+import os
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+# Create your views here.
+
+def post_list1(request):
+    type = 'http response type'
+    return HttpResponse('''
+        <h1>View Test Page</h1>
+        <p>{type}</p>
+        <p>{type} 테스트 페이지입니다.</p>'''.format(type=type)) 
+
