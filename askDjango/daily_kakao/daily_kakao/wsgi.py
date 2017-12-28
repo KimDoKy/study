f21e0c5cc2cd80661ec004567b729729c7fa1ec5
Kim Do Ky <makingfunk0@gmail.com>
2017-12-28 18:04:37 +0900

    임시파일 정리, kakao_daily requirement 분기

--- askDjango/daily_kakao/daily_kakao/wsgi.py
+++ askDjango/daily_kakao/daily_kakao/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "daily_kakao.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "daily_kakao.settingsi.prod")
 
 application = get_wsgi_application()
