edfa2f022966292e1437f226a775ea4378111873
Kim Do Ky <makingfunk0@gmail.com>
2017-12-29 00:09:55 +0900

    Post 모델 생성/마이그레이션, 어드민등록

--- askDjango/travel_blog/travel_blog/blog/models.py
+++ askDjango/travel_blog/travel_blog/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.forms import ValidationError
+import re
 
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LagLat Type')
 
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name='제목')
+    content = models.TextField(help_text='Mark 문법으로 작성하세요.')
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50, validators=[lnglat_validator], help_text='경도/위도 포맷으로 입력하세요.', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
