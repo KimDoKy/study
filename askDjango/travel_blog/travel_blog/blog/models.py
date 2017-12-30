01a919e96ce42c4e704813955fa3e2f844579681
Kim Do Ky <makingfunk0@gmail.com>
2017-12-30 21:35:39 +0900

    blog models Post 모델에 status 필드를 추가, 이를 처리하는 Custom Admin Action 구현

--- askDjango/travel_blog/travel_blog/blog/models.py
+++ askDjango/travel_blog/travel_blog/blog/models.py
@@ -7,10 +7,17 @@ def lnglat_validator(value):
         raise ValidationError('Invalid LagLat Type')
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+        ('w', 'Withdrawn'),
+    )
+
     author = models.CharField(max_length=20)
     title = models.CharField(max_length=100, verbose_name='제목')
     content = models.TextField(help_text='Mark 문법으로 작성하세요.')
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, validators=[lnglat_validator], help_text='경도/위도 포맷으로 입력하세요.', blank=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
