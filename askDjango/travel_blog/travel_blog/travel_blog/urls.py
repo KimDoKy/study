62100186b0a20ed9643383f74e6d50c9e2daf6f1
Kim Do Ky <makingfunk0@gmail.com>
2018-01-03 21:53:54 +0900

    root url redirect 처리

--- askDjango/travel_blog/travel_blog/travel_blog/urls.py
+++ askDjango/travel_blog/travel_blog/travel_blog/urls.py
@@ -1,12 +1,16 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
+def root(request):
+    return redirect('blog:post_list')
 
 urlpatterns = [
+    path('', root, name='root'),
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('blog/', include('blog.urls', namespace='blog')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
 
 if settings.DEBUG:
