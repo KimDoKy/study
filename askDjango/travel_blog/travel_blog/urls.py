245a094eacdf64d1548cf37d7d9453a6614f5671
Kim Do Ky <makingfunk0@gmail.com>
2018-01-09 20:18:07 +0900

    media 파일 서빙 지원

--- askDjango/travel_blog/travel_blog/urls.py
+++ askDjango/travel_blog/travel_blog/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', lambda r: redirect('blog:post_list'), name='root'),
@@ -9,6 +10,8 @@ urlpatterns = [
     path('blog/', include('blog.urls', namespace='blog')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
