d723a53641b481d2f2ababe5c85ddf6e41d7c462
Kim Do Ky <makingfunk0@gmail.com>
2018-01-10 13:21:25 +0900

    imagekit을 활용하여, 이미지가 업로드될 때, 썸네일 처리된 이미지만 저장

--- askDjango/travel_blog/blog/models.py
+++ askDjango/travel_blog/blog/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.urls import reverse
 from django.forms import ValidationError
 import re
-from imagekit.models import ImageSpecField
+from imagekit.models import ProcessedImageField
 from imagekit.processors import Thumbnail
 
 def lnglat_validator(value):
@@ -20,9 +20,9 @@ class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, verbose_name='제목')
     content = models.TextField(help_text='Mark 문법으로 작성하세요.')
-    photo = models.ImageField(blank=True, upload_to='blog/post/%Y/%m/%d')
-    photo_thumbnail = ImageSpecField(source='photo',
-            processors = [Thumbnail(300, 300)],
+    photo = ProcessedImageField(blank=True, upload_to='blog/post/%Y/%m/%d',
+            processors=[Thumbnail(300, 300)],
+            format='JPEG',
             options={'quality': 60})
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, validators=[lnglat_validator], help_text='경도/위도 포맷으로 입력하세요.', blank=True)
