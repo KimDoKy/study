2453a6164efa83ffba72bd2e8731402ad1747b6c
Kim Do Ky <makingfunk0@gmail.com>
2018-01-12 13:29:53 +0900

    로그인/로그아웃 구현

--- askDjango/travel_blog/accounts/urls.py
+++ askDjango/travel_blog/accounts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from django.conf import settings
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('profile/', views.profile),
+    path('login/', auth_views.login, name='login',
+         kwargs={'template_name': 'accounts/login_form.html'}),
+    path('logout/', auth_views.logout, name='logout',
+         kwargs={'next_page': settings.LOGIN_URL}),
+    path('profile/', views.profile, name='profile'),
 ]
