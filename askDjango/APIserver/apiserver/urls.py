05ebb545b263c467118bf02df18aeb69dad96870
Kim Do Ky <makingfunk0@gmail.com>
2018-01-26 22:45:02 +0900

    added apiview app

--- askDjango/APIserver/apiserver/urls.py
+++ askDjango/APIserver/apiserver/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('sample/', include('sample.urls', namespace='sample')),
+	path('apiview/', include('apiview.urls', namespace='apiview')),
 ]
