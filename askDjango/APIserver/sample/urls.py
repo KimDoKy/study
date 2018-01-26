42b950d39d5905e7c9c26a14481ac24aa78a5b22
Kim Do Ky <makingfunk0@gmail.com>
2018-01-26 22:14:47 +0900

    ModelViewSet과 ListAPIView 차이

--- askDjango/APIserver/sample/urls.py
+++ askDjango/APIserver/sample/urls.py
@@ -5,9 +5,10 @@ from . import views
 
 app_name = 'sample'
 
-router = DefaultRouter()
-router.register('post', views.PostViewSet)
+# router = DefaultRouter()
+# router.register('post', views.PostViewSet)
 
 urlpatterns = [
-	path('', include(router.urls)),
+# 	path('', include(router.urls)),
+	path('post/', views.PostList.as_view()),
 	]
