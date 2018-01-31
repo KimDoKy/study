87bb83962df9a884b0daf5c095011000a51568fe
Kim Do Ky <makingfunk0@gmail.com>
2018-01-31 23:14:39 +0900

    apiview Post API, generics 활용

--- askDjango/APIserver/apiview/views.py
+++ askDjango/APIserver/apiview/views.py
@@ -1,67 +1,12 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import generics
-from rest_framework import mixins
 from .models import Post
 from .serializers import PostSerializer
 
 
-class PostListAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class PostListAPIView(generics.ListCreateAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostSerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
-
-	def post(self, request, *args, **kwargs):
-		return self.create(request, *args, **kwargs) 
-
-# /post/10/ => GET, PUT, DELETE
-class PostDetailAPIView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
-
+class PostDetailAPIView(generics.RetrieveUpdateDestroyAPIView):	
 	queryset = Post.objects.all()
 	serializer_class = PostSerializer
-
-	def get(self, request, *args, **kwargs):
-		return self.retrieve(request, *args, **kwargs)
-
-	def put(self, request, *args, **kwargs):
-		return self.update(request, *args, **kwargs)
-	
-	def delete(self, request, *args, **kwargs):
-		return self.destroy(request, *args, **kwargs)
-
-from rest_framework import status
-from .models import Post
-from .serializers import PostSerializer
-from rest_framework.decorators import api_view
-
-@api_view(['GET', 'POST'])
-def post_list(request):
-	if request.method == 'GET':
-		serializer = PostSerializer(Post.objects.all(), many=True)
-		return Response(serializer.data)
-	else:
-		serializer = PostSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=201)
-		return Response(serializer.errors, status=400)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def post_detail(request, pk):
-	post = get_object_or_404(Post, pk=pk)
-
-	if request.method == 'GET':
-	    serializer = PostSerializer(post)
-	    return Response(serializer.data)
-	elif request.method == 'PUT':
-	    serializer = PostSerializer(post, data=request.data)
-	    if serializer.is_valid():
-		    serializer.save()
-		    return Response(serializer.data)
-	    return Response(serializer.errors, status=400)
-	else:
-		post.delete()
-		return Response(status=204)
