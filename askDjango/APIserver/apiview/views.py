bf095ed18fee68cb918e18d3272dc5ccae704a03
Kim Do Ky <makingfunk0@gmail.com>
2018-01-31 21:57:46 +0900

    apiview Post FBV List/Detail API 구현

--- askDjango/APIserver/apiview/views.py
+++ askDjango/APIserver/apiview/views.py
@@ -40,3 +40,37 @@ class PostDetailAPIView(APIView):
 		post = self.get_object(pk)
 		post.delete()
 		return Response(status=204)
+
+from rest_framework import status
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.decorators import api_view
+
+@api_view(['GET', 'POST'])
+def post_list(request):
+	if request.method == 'GET':
+		serializer = PostSerializer(Post.objects.all(), many=True)
+		return Response(serializer.data)
+	else:
+		serializer = PostSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=201)
+		return Response(serializer.errors, status=400)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+
+	if request.method == 'GET':
+	    serializer = PostSerializer(post)
+	    return Response(serializer.data)
+	elif request.method == 'PUT':
+	    serializer = PostSerializer(post, data=request.data)
+	    if serializer.is_valid():
+		    serializer.save()
+		    return Response(serializer.data)
+	    return Response(serializer.errors, status=400)
+	else:
+		post.delete()
+		return Response(status=204)
