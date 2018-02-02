968cc3d2e238330b1508c21b4377ef069f0cd1f8
Kim Do Ky <makingfunk0@gmail.com>
2018-02-02 10:32:03 +0900

    Post Detail 뷰, 템플릿, url 구현

--- diary/daily_practice/daily/blog/urls.py
+++ diary/daily_practice/daily/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import post_list
+from .views import post_list, post_detail
 
 app_name = 'blog'
 urlpatterns = [
 	path('', post_list, name='post_list'),
+	path('<pk>/', post_detail, name='post_detail'),
 	]
--- diary/daily_practice/daily/blog/views.py
+++ diary/daily_practice/daily/blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
 	post = Post.objects.all()
 	return render(request, 'blog/post_list.html', {'post_list':post})
+
+def post_detail(request, pk):
+	qs = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post':qs})
