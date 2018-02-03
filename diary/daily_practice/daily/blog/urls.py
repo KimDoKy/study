e3f79fe26e0ce71081eae2284bfc5e7a5b1ca52d
Kim Do Ky <makingfunk0@gmail.com>
2018-02-04 00:03:12 +0900

    Post create 구현

--- diary/daily_practice/daily/blog/urls.py
+++ diary/daily_practice/daily/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import post_list, post_detail
+from .views import post_list, post_detail, post_new
 
 app_name = 'blog'
 urlpatterns = [
 	path('', post_list, name='post_list'),
+    path('new/', post_new, name='post_new'),
 	path('<pk>/', post_detail, name='post_detail'),
 	]
