27b229ccea2cd20908b7b553cfac41142a8476bc
Kim Do Ky <makingfunk0@gmail.com>
2018-02-03 13:51:18 +0900

    Post 검색 구현

--- diary/daily_practice/daily/blog/views.py
+++ diary/daily_practice/daily/blog/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
-	post = Post.objects.all()
-	return render(request, 'blog/post_list.html', {'post_list':post})
+	qs = Post.objects.all()
+	q = request.GET.get('q', '')
+	if q:
+		qs = qs.filter(title__icontains=q)
+	return render(request, 'blog/post_list.html', {'post_list':qs, 'q':q})
 
 def post_detail(request, pk):
 	qs = get_object_or_404(Post, pk=pk)
