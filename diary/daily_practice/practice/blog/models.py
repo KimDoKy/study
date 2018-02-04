f10eded5ce329a6429a7bd8863f02ca58668bb52
Kim Do Ky <makingfunk0@gmail.com>
2018-02-04 18:33:58 +0900

    blog Post 모델 생성 및 마이그레이트

--- diary/daily_practice/practice/blog/models.py
+++ diary/daily_practice/practice/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', args=[self.id])
