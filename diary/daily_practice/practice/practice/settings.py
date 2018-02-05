b8f0baa3fc17c28d88539fac24c91ce21b626c3e
Kim Do Ky <makingfunk0@gmail.com>
2018-02-05 13:23:21 +0900

    media 설정 추가

--- diary/daily_practice/practice/practice/settings.py
+++ diary/daily_practice/practice/practice/settings.py
@@ -15,7 +15,6 @@ import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(BASE_DIR)
 
 CONF_DIR = os.path.join(BASE_DIR, '.secret')
 CONF_FILE = json.loads(open(os.path.join(CONF_DIR, 'common.json')).read())
@@ -123,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
