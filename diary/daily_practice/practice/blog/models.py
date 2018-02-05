5339dd226c53d93f0c83327fcadd697e3102d98a
Kim Do Ky <makingfunk0@gmail.com>
2018-02-05 13:14:38 +0900

    Post 모델 생성, 마이크레이션

--- diary/daily_practice/practice/blog/models.py
+++ diary/daily_practice/practice/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    photo = models.ImageField(blank=True, upload_to='media')
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail')
