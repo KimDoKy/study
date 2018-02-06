fffb0e53238f45d26e7caa727856969d0ef33afa
Kim Do Ky <makingfunk0@gmail.com>
2018-02-06 23:33:52 +0900

    post_new 구현

--- daily_practice/blog/views.py
+++ daily_practice/blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
-
+from .forms import PostForm
 
 def post_list(request):
     qs = Post.objects.all()
@@ -9,3 +9,13 @@ def post_list(request):
 def post_detail(request, pk):
     qs = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post':qs})
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form':form})
