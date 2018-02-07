88807b280b9cb412125839eb011cfb4e90ce44d3
Kim Do Ky <makingfunk0@gmail.com>
2018-02-07 22:34:07 +0900

    Post 모델에 user 추가

--- daily_practice/blog/models.py
+++ daily_practice/blog/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.conf import settings
 from django.urls import reverse
 
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     content = models.TextField()
     create_at = models.DateTimeField(auto_now_add=True)
