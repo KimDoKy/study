25eae6ac6edd4dfa6d00954a5fa000d1ca95b00e
Kim Do Ky <makingfunk0@gmail.com>
2018-02-09 23:48:32 +0900

    post_new 구현

--- daily_practice/blog/models.py
+++ daily_practice/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
