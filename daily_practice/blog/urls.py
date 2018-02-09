ece28e6a6adc514535c096ca7cdc8a30149a20a5
Kim Do Ky <makingfunk0@gmail.com>
2018-02-09 23:53:02 +0900

    post_eidt 구현

--- daily_practice/blog/urls.py
+++ daily_practice/blog/urls.py
@@ -5,6 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('edit/', views.post_new),
+    path('new/', views.post_new),
+    path('<pk>/edit/', views.post_edit),
     path('<pk>/', views.post_detail, name='post_detail'),
     ]
