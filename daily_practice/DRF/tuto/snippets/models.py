fd5110e47046c0571ebeddd296a76c75f65c09ec
Kim Do Ky <makingfunk0@gmail.com>
2018-02-11 17:16:40 +0900

    serializer 클래스 생성, 모델에 오타 수정

--- daily_practice/DRF/tuto/snippets/models.py
+++ daily_practice/DRF/tuto/snippets/models.py
@@ -3,8 +3,8 @@ from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICE = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICE = sorted((item, item) for item in get_all_styles())
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
  
 class Snippet(models.Model):
@@ -12,8 +12,8 @@ class Snippet(models.Model):
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
     linenos = models.BooleanField(default=False)
-    language = models.CharField(choices=LANGUAGE_CHOICE, default='python', max_length=100)
-    style = models.CharField(choices=STYLE_CHOICE, default='friendly', max_length=100)
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
 
     class Meta:
         ordering = ('created',)
