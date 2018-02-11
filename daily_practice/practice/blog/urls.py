8852605f0c2843c90db8c5ea7e2923c19580bdc8
Kim Do Ky <makingfunk0@gmail.com>
2018-02-11 15:37:00 +0900

    post_new 구현

--- daily_practice/practice/blog/urls.py
+++ daily_practice/practice/blog/urls.py
@@ -5,5 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('new/', views.post_new),
     path('<pk>/', views.post_detail, name='post_detail'),
     ]
