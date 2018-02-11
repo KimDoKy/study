cd5f44f1c40da17c9e5c6f00869dc6af55311311
Kim Do Ky <makingfunk0@gmail.com>
2018-02-11 23:43:40 +0900

    API의 로그인 뷰와 로그아웃 뷰에 사용되는 패턴 추가

--- daily_practice/DRF/tuto/snippets/urls.py
+++ daily_practice/DRF/tuto/snippets/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
+from django.conf.urls import include
 
 urlpatterns = [
     path('', views.SnippetList.as_view()),
     path('<pk>/', views.SnippetDetail.as_view()),
+    path('user/', views.UserList.as_view()),
+    path('user/<pk>/', views.UserDetail.as_view()),
     ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    ]
