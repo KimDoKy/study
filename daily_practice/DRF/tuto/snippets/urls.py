6f1f08878deaf833292ea2772abcb02f903f9ed3
Kim Do Ky <makingfunk0@gmail.com>
2018-02-12 13:21:24 +0900

    뷰셋과 주소를 명시적으로 연결하기

--- daily_practice/DRF/tuto/snippets/urls.py
+++ daily_practice/DRF/tuto/snippets/urls.py
@@ -3,13 +3,35 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 from django.conf.urls import include
 
+from .views import SnippetViewSet, UserViewSet, api_root
+from rest_framework import renderers
+
+snippet_list = SnippetViewSet.as_view({
+        'get': 'list',
+        'post': 'create',
+        })
+snippet_detail = SnippetViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'patch': 'partial_update',
+        'delete': 'destroy',
+        })
+snippet_highlight = SnippetViewSet.as_view({
+        'get': 'highlight'}, renderer_classes=[renderers.StaticHTMLRenderer])
+user_list = UserViewSet.as_view({
+        'get': 'list'
+        })
+user_detail = UserViewSet.as_view({
+        'get': 'retrieve'
+        })
+
 urlpatterns = [
     path('', views.api_root),
-    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
-    path('snippets/<pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('snippets/', snippet_list, name='snippet-list'),
+    path('snippets/<pk>/', snippet_detail, name='snippet-detail'),
+    path('snippets/<pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+    path('users/', user_list, name='user-list'),
+    path('users/<pk>/', user_detail, name='user-detail'),
     ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
