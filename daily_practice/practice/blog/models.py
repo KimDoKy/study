4e5dc57d677f46b047cbc0fff4208d54f6700cbc
Kim Do Ky <makingfunk0@gmail.com>
2018-02-12 23:18:43 +0900

    Post 모델에 get_absolute_url 설정

--- daily_practice/practice/blog/models.py
+++ daily_practice/practice/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -8,3 +9,9 @@ class Post(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='media')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
