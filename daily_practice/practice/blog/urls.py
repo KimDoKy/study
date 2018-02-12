7b20e0491cd604637dd6de8f3f169a4b572e3f84
Kim Do Ky <makingfunk0@gmail.com>
2018-02-12 23:54:48 +0900

    post_del 구현

--- daily_practice/practice/blog/urls.py
+++ daily_practice/practice/blog/urls.py
@@ -6,6 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('new/', views.post_new),
+    path('<pk>/del/', views.post_del),
     path('<pk>/edit/', views.post_edit),
     path('<pk>/', views.post_detail, name='post_detail'),
     ]
