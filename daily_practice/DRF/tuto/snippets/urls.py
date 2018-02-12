8c459022d8bd687cedb1bd63d12c60486f3711a8
Kim Do Ky <makingfunk0@gmail.com>
2018-02-12 13:01:10 +0900

    url 패턴에 이름 붙이기

--- daily_practice/DRF/tuto/snippets/urls.py
+++ daily_practice/DRF/tuto/snippets/urls.py
@@ -4,10 +4,12 @@ from . import views
 from django.conf.urls import include
 
 urlpatterns = [
-    path('', views.SnippetList.as_view()),
-    path('<pk>/', views.SnippetDetail.as_view()),
-    path('user/', views.UserList.as_view()),
-    path('user/<pk>/', views.UserDetail.as_view()),
+    path('', views.api_root),
+    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
+    path('snippets/<pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    path('snippets/<pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<pk>/', views.UserDetail.as_view(), name='user-detail'),
     ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
