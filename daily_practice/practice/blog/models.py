0bfd6e4812dd5755e7764735f6d1575d148b81a0
Kim Do Ky <makingfunk0@gmail.com>
2018-02-13 00:01:07 +0900

    Comment 추가

--- daily_practice/practice/blog/models.py
+++ daily_practice/practice/blog/models.py
@@ -15,3 +15,9 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.id])
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    message = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
