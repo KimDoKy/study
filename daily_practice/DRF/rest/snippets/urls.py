42830301e228999c05e802116c07b0a7bf0eef27
Kim Do Ky <makingfunk0@gmail.com>
2018-02-13 23:35:15 +0900

    serializer 사용하는 django 뷰

--- daily_practice/DRF/rest/snippets/urls.py
+++ daily_practice/DRF/rest/snippets/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 app_name = 'snippets'
 
-urlpatterns = []
+urlpatterns = [
+    path('snippets/', views.snippet_list),
+    path('snippets/<pk>/', views.snippet_detail),
+]
