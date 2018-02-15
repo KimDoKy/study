ef1294d212e459b061518494d9338f97bac1fce9
Kim Do Ky <makingfunk0@gmail.com>
2018-02-15 15:23:13 +0900

    인증과 권한/ 사용자가 만든 코드 조각 연결, 시리얼라이저 업데이트, 뷰에 요청 권한 추가, 탐색 가능한 API에 로그인 추가, 객체 수준에서 권한 설정

--- daily_practice/DRF/rest/snippets/urls.py
+++ daily_practice/DRF/rest/snippets/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
@@ -12,3 +12,7 @@ urlpatterns = [
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    ]
--- daily_practice/DRF/rest/snippets/views.py
+++ daily_practice/DRF/rest/snippets/views.py
@@ -2,14 +2,21 @@ from .models import Snippet
 from django.contrib.auth.models import User
 from .serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
+from rest_framework import permissions
+from .permissions import IsOwnerOrReadOnly
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly,)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
