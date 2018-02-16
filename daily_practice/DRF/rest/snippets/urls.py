f4dc9c3ac0a1a671d0183e84d7c6b558067f53a2
Kim Do Ky <makingfunk0@gmail.com>
2018-02-16 22:04:52 +0900

    api 최상단에 대한 엔드 포인트, 조각의 하이라이트 버전에 대한 엔드포인트

--- daily_practice/DRF/rest/snippets/urls.py
+++ daily_practice/DRF/rest/snippets/urls.py
@@ -5,7 +5,9 @@ from . import views
 app_name = 'snippets'
 
 urlpatterns = [
+    path('', views.api_root),
     path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<pk>/highlight/', views.SnippetHighlight.as_view()),
     path('snippets/<pk>/', views.SnippetDetail.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<pk>/', views.UserDetail.as_view()),
