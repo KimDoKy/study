b89edf6060a8e370216dd664a96bedcb3923b9c2
Kim Do Ky <makingfunk0@gmail.com>
2018-02-21 20:16:45 +0900

    mega/ allauth

--- daily_practice/mega_tuto/mega_tuto/urls.py
+++ daily_practice/mega_tuto/mega_tuto/urls.py
@@ -3,8 +3,13 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('page.urls')),
+
+    # User management
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
+
+    # django admin
     path('admin/', admin.site.urls),
 
 ]
