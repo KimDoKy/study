8aee12bd41ce27f361b09057cc62531eebbe5c82
Kim Do Ky <makingfunk0@gmail.com>
2018-02-27 13:05:56 +0900

    URL 패턴에 이름 붙이기

--- daily_practice/drf/snippets/urls.py
+++ daily_practice/drf/snippets/urls.py
@@ -2,16 +2,14 @@ from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
-urlpatterns = [
+urlpatterns = format_suffix_patterns([
     path('', views.api_root),
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<pk>/highlight/', views.SnippetHighlight.as_view()),
-    path('snippets/<pk>/', views.SnippetDetail.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<pk>/', views.UserDetail.as_view()),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
+    path('snippets/<pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    path('snippets/<pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<pk>/', views.UserDetail.as_view(), name='user-detail'),
+])
 
 urlpatterns += [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
