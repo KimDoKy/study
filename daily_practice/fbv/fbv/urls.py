8ee311a0c8989196a6eb68b682961102ae22b885
Kim Do Ky <makingfunk0@gmail.com>
2018-02-27 16:45:35 +0900

    create post_detail, settings url root

--- daily_practice/fbv/fbv/urls.py
+++ daily_practice/fbv/fbv/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog')),
+    path('', include('blog.urls', namespace='blog')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
