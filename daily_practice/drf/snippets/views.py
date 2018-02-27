2598b8a449f30c5cee0c64b052ad1caad899bff6
Kim Do Ky <makingfunk0@gmail.com>
2018-02-27 13:14:59 +0900

    뷰셋을 사용하여 리팩터링

--- daily_practice/drf/snippets/views.py
+++ daily_practice/drf/snippets/views.py
@@ -1,6 +1,5 @@
 from .models import Snippet
 from .serializers import SnippetSerializer
-from rest_framework import generics
 
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
@@ -8,19 +7,13 @@ from snippets.serializers import UserSerializer
 from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, detail_route
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 from rest_framework import renderers
 
-class SnippetHighlight(generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    renderer_classes = (renderers.StaticHTMLRenderer,)
-
-    def get(self, request, *args, **kwargs):
-        snippet = self.get_object()
-        return Response(snippet.highlighted)
+from rest_framework import viewsets
 
 @api_view(['GET',])
 def api_root(request, format=None):
@@ -29,23 +22,20 @@ def api_root(request, format=None):
             'snippets': reverse('snippet-list', request=request, format=format)
             })
 
-class SnippetList(generics.ListCreateAPIView):
+class SnippetViewSet(viewsets.ModelViewSet):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
-
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer()
-
-class UserDetail(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
