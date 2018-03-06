6a073137fc28161e7877aa80a72f8800b483fc2a
Kim Do Ky <makingfunk0@gmail.com>
2018-03-06 12:45:26 +0900

    add post_edit

--- daily_practice/SignUp/blog/urls.py
+++ daily_practice/SignUp/blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('new/', views.post_new, name='post_new'),
+    path('<pk>/edit/', views.post_edit, name='post_edit'),
     path('<pk>/', views.post_detail, name='post_detail'),
 ]
