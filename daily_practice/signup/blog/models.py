bfb5d6e311d32dc26d75d5d2366693a8a3ae4cf1
Kim Do Ky <makingfunk0@gmail.com>
2018-03-08 20:54:40 +0900

    modify photo model. blank true, admin register post model

--- daily_practice/signup/blog/models.py
+++ daily_practice/signup/blog/models.py
@@ -6,8 +6,11 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     content = models.TextField()
-    photo = models.ImageField()
+    photo = models.ImageField(blank=True)
     create_at = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.id])
+
+    def __str__(self):
+        return self.title
