f32640541c347fc01b57f12d3e6fb46d6dc638aa
Kim Do Ky <makingfunk0@gmail.com>
2018-03-08 20:40:39 +0900

    migrate post model

--- daily_practice/signup/blog/models.py
+++ daily_practice/signup/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    photo = models.ImageField()
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
