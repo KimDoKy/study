882218181171ae36649ea8321239a77cb2b872de
Kim Do Ky <makingfunk0@gmail.com>
2018-03-10 00:38:45 +0900

    add post_new

--- daily_practice/signUp/blog/views.py
+++ daily_practice/signUp/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     qs = Post.objects.all()
@@ -11,3 +12,13 @@ def post_list(request):
 def post_detail(request, pk):
     qs = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post':qs})
+
+def post_new(request):
+    if request.method == 'POST':
+        forms = PostForm(request.POST, request.FILES)
+        if forms.is_valid():
+            post = forms.save()
+            return redirect(post)
+    else:
+        forms = PostForm()
+    return render(request, 'blog/post_form.html', {'form':forms})
