2959366584d695119ce6c2ebf05b715943c0919b
Kim Do Ky <makingfunk0@gmail.com>
2018-03-13 13:16:18 +0900

    settings blog urls, add post_list

--- daily_practice/sing_up/blog/models.py
+++ daily_practice/sing_up/blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     content = models.TextField()
-    photo = models.ImageField()
+    photo = models.ImageField(blank=True)
     create_at = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
--- daily_practice/sing_up/sing_up/settings.py
+++ daily_practice/sing_up/sing_up/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'sing_up.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
