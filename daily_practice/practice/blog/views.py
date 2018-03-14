c17b3ce2ce52333df9caf168391635ee7150a112
Kim Do Ky <makingfunk0@gmail.com>
2018-03-14 21:18:11 +0900

    add post_new(CBV)

--- daily_practice/practice/blog/views.py
+++ daily_practice/practice/blog/views.py
@@ -1,6 +1,8 @@
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 post_list = ListView.as_view(model=Post)
 
 post_detail = DetailView.as_view(model=Post)
+
+post_new = CreateView.as_view(model=Post, fields='__all__')
