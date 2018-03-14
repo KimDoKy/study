1b89b73abcd3c75d5a00888640b5652d0daea480
Kim Do Ky <makingfunk0@gmail.com>
2018-03-14 20:12:04 +0900

    add post_new(CreateView)

--- daily_practice/CBV/blog/urls.py
+++ daily_practice/CBV/blog/urls.py
@@ -5,6 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('new/', views.post_new, name='post_new'),
     path('<pk>/', views.post_detail, name='post_detail'),
     ]
 
--- daily_practice/CBV/blog/views.py
+++ daily_practice/CBV/blog/views.py
@@ -1,7 +1,9 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from django.conf import settings
 from .models import Post
 
 post_list = ListView.as_view(model=Post)
 
 post_detail = DetailView.as_view(model=Post)
+
+post_new = CreateView.as_view(model=Post, fields='__all__')
