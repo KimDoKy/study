68aa4a068e208708c5a388bd6c894bb6ed3d7e1b
Kim Do Ky <makingfunk0@gmail.com>
2018-03-14 21:20:06 +0900

    add post_edit(CBV)

--- daily_practice/practice/blog/views.py
+++ daily_practice/practice/blog/views.py
@@ -1,8 +1,10 @@
 from .models import Post
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 post_list = ListView.as_view(model=Post)
 
 post_detail = DetailView.as_view(model=Post)
 
 post_new = CreateView.as_view(model=Post, fields='__all__')
+
+post_edit = UpdateView.as_view(model=Post, fields='__all__')
