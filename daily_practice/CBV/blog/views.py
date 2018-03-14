1bb1989c7c0f7f070819b60f147e99908941d6e5
Kim Do Ky <makingfunk0@gmail.com>
2018-03-14 20:24:32 +0900

    add post_del(DeleteView)

--- daily_practice/CBV/blog/views.py
+++ daily_practice/CBV/blog/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.conf import settings
 from .models import Post
+from django.urls import reverse_lazy
 
 post_list = ListView.as_view(model=Post)
 
@@ -9,3 +10,5 @@ post_detail = DetailView.as_view(model=Post)
 post_new = CreateView.as_view(model=Post, fields='__all__')
 
 post_edit = UpdateView.as_view(model=Post, fields='__all__')
+
+post_del = DeleteView.as_view(model=Post, success_url=reverse_lazy('blog:post_list'))
