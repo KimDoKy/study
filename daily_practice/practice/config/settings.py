643a9291e3e025b15adb29815a8e1a4b37a597ab
Kim Do Ky <makingfunk0@gmail.com>
2018-03-14 20:48:05 +0900

    change conf dir name

--- daily_practice/practice/config/settings.py
+++ daily_practice/practice/config/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'practice.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'practice.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
