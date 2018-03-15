f46e9294f2dbd7485e04c7e6f232885435ee6fbb
Kim Do Ky <makingfunk0@gmail.com>
2018-03-15 23:13:57 +0900

    add post_list, settings blog url

--- daily_practice/fbv/blog/views.py
+++ daily_practice/fbv/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    qs = Post.objects.all()
+    q = request.GET.get('q')
+    if q:
+        qs = qs.filter(title__icontains=q)
+    return render(request, 'blog/post_list.html', {'posts':qs})
