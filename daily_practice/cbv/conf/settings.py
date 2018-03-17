d19196bd15376933652e270af57b7dc5c61ba993
Kim Do Ky <makingfunk0@gmail.com>
2018-03-17 23:07:17 +0900

    add signup

--- daily_practice/cbv/conf/settings.py
+++ daily_practice/cbv/conf/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = '/blog/'
+LOGOUT_REDIRECT_URL = '/'
