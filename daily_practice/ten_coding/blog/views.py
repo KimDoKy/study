0849d973c44d1184b686671c6cef801f613dffb2
Kim Do Ky <makingfunk0@gmail.com>
2018-03-19 00:12:03 +0900

    add post_new

--- daily_practice/ten_coding/blog/views.py
+++ daily_practice/ten_coding/blog/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 post_list = ListView.as_view(model=Post)
 
 post_detail = DetailView.as_view(model=Post)
+
+post_new = CreateView.as_view(model=Post, fields='__all__')
