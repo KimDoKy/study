32d0c58914e22f40678567bbd645813fb7050aab
Kim Do Ky <makingfunk0@gmail.com>
2018-03-22 21:17:49 +0900

    save gps, test print

--- daily_practice/GPS/album/views.py
+++ daily_practice/GPS/album/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Album
+from util.gps import get_gps
+from django.conf import settings
 
-# Create your views here.
+
+def photo_list(request):
+    qs = Album.objects.all()
+    api_key = settings.MAP_API
+    for instance in qs:
+        instance.lat, instance.lng = get_gps(instance.photo.path)
+        instance.save()
+    return render(request, 'album/photo_list.html', {
+            'qs':qs,
+            'api':api_key,
+            })
+            
