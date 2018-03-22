9ac9e6abdaabf2aa18a6dbb3c8a02b6b9f2b1b5e
Kim Do Ky <makingfunk0@gmail.com>
2018-03-22 21:15:27 +0900

    divisino api_key, settings templates

--- daily_practice/GPS/conf/settings.py
+++ daily_practice/GPS/conf/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'conf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'medis')
+
+MAP_API = CONF_FILE['google_map']['api_key']
