35b36d1f789e17c187311cc7d595e326dd6d46ad
Kim Do Ky <makingfunk0@gmail.com>
2018-03-24 01:36:21 +0900

    연습을 위한 초기화

--- daily_practice/GPS/conf/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic.base import TemplateView
-from django.conf import settings
-
-class Home(TemplateView):
-    template_name = 'home.html'
-
-    def get_context_data(self, **kwargs):
-        context = {'api':settings.MAP_API}
-        return context
