fb04eb89f28fae3fd9a2b31e4cbac7a5ad5fc541
Kim Do Ky <makingfunk0@gmail.com>
2018-03-24 23:51:23 +0900

    create model Album/Photo, registed model

--- daily_practice/gps_map/album/admin.py
+++ daily_practice/gps_map/album/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Photo, Album
 
-# Register your models here.
+class PhotoInline(admin.TabularInline):
+    model = Photo
+
+class AlbumAdmin(admin.ModelAdmin):
+    inlines = [
+        PhotoInline,
+        ]
+
+admin.site.register(Photo)
+admin.site.register(Album, AlbumAdmin)
--- daily_practice/gps_map/album/models.py
+++ daily_practice/gps_map/album/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Photo(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    photo = models.ImageField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    lat = models.FloatField(blank=True, null=True)
+    lng = models.FloatField(blank=True, null=True)
+    album = models.ForeignKey('Album', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
+class Album(models.Model):
+    title = models.CharField(max_length=50)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
