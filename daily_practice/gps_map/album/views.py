0e4a22eb906fff996ec389f919fc290a5a2a6a8d
Kim Do Ky <makingfunk0@gmail.com>
2018-03-25 14:32:00 +0900

    add album_view

--- daily_practice/gps_map/album/views.py
+++ daily_practice/gps_map/album/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Photo, Album
+from django.views.generic import ListView
+from util.gps import get_gps
 
-# Create your views here.
+AlbumView = ListView.as_view(model=Album, template_name='album_list.html')
