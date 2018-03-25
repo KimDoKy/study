224fb6a975a5ee89a090ac31da47a5f2d936d6b8
Kim Do Ky <makingfunk0@gmail.com>
2018-03-25 16:08:47 +0900

    running a function get_gps when run album_detail

--- daily_practice/gps_map/album/views.py
+++ daily_practice/gps_map/album/views.py
@@ -6,7 +6,15 @@ from util.gps import get_gps
 AlbumView = ListView.as_view(model=Album, template_name='album_list.html')
 
 class PhotoDetailView(DetailView):
-    model = Photo
-    template_name = 'album/photo_list.html'
+    model = Album
+    template_name = 'album/album_detail.html'
 
-PhotoView = DetailView.as_view(model=Album)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['photo_list'] = Photo.objects.filter(album_id=self.kwargs['pk'])
+        for instance in context['photo_list']:
+            instance.lat, instance.lng = get_gps(instance.photo.path)
+            instance.save()
+        return context
+
+PhotoView = PhotoDetailView.as_view()
