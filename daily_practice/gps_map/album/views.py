304ae00f19de0e7bf757a5e1937bb5f0a8152024
Kim Do Ky <makingfunk0@gmail.com>
2018-03-25 17:07:41 +0900

    add google map

--- daily_practice/gps_map/album/views.py
+++ daily_practice/gps_map/album/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Photo, Album
 from django.views.generic import ListView, DetailView
 from util.gps import get_gps
+from django.conf import settings
 
 AlbumView = ListView.as_view(model=Album, template_name='album_list.html')
 
@@ -12,6 +13,7 @@ class PhotoDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['photo_list'] = Photo.objects.filter(album_id=self.kwargs['pk'])
+        context['api'] = settings.GOOGLE_API
         for instance in context['photo_list']:
             instance.lat, instance.lng = get_gps(instance.photo.path)
             instance.save()
