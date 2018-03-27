4578e8068fe372f61c549219943a6a68dc48e6af
Kim Do Ky <makingfunk0@gmail.com>
2018-03-27 13:28:59 +0900

    drf clean dir

--- Blog_ex/python/library/chap11/7_pytest/test_sample3.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-@pytest.mark.parametrize("obj", ['1', '2', 'Foo'])
-def test_indigit(obj):
-    assert obj.isdigit()
--- askDjango/daily_kakao/daily_kakao/storages.py
+++
@@ -1,7 +0,0 @@
-from storages.backends.azure_storage import AzureStorage
-
-class StaticStorage(AzureStorage):
-    azure_container = 'static'
-
-class MediaStorage(AzureStorage):
-    azure_container = 'media'
