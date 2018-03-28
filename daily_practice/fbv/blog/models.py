d0bd65003dba68031271792b6bd56ffc6d49991b
Kim Do Ky <makingfunk0@gmail.com>
2018-03-28 11:53:04 +0900

    migrate Post model

--- daily_practice/fbv/blog/models.py
+++ daily_practice/fbv/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length = 50)
+    photo = models.ImageField(blank=True)
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
