5845f23f63d2e74ab44e3964d4b89ee9c5363b7b
Kim Do Ky <makingfunk0@gmail.com>
2018-03-28 09:58:06 +0900

    create another

--- askDjango/travel_blog/.sample/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ViewSampleConfig(AppConfig):
-    name = 'view_sample'
--- askDjango/travel_blog/blog/context_processors.py
+++
@@ -1,6 +0,0 @@
-from django.utils import timezone
-
-def blog(request):
-	return {
-		'current_datetime': timezone.now(),
-	}
