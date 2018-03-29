28cf20b41475dfd3e86d376e3b292e15b2f60216
Kim Do Ky <makingfunk0@gmail.com>
2018-03-29 21:29:40 +0900

    cbv / create accounts app

--- daily/cbv/conf/settings.py
+++ daily/cbv/conf/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
     'blog',
 ]
 
@@ -125,3 +126,6 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
