e793665bcef4c474ddd44d08ccc44aba4bcd1c3a
Kim Do Ky <makingfunk0@gmail.com>
2018-03-29 21:11:21 +0900

    cbv / add post_new

--- daily/cbv/blog/views.py
+++ daily/cbv/blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
-post_list = ListView.as_view(model=Post, template_name='post_list.html')
+post_list = ListView.as_view(model=Post)
 
-post_detail = DetailView.as_view(model=Post, template_name='blog/post_detail.html')
+post_detail = DetailView.as_view(model=Post)
+
+post_new = CreateView.as_view(model=Post, fields='__all__')
