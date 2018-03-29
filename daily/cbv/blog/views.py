b61c103228d9a2181dc4536ab2c81cbdb4d2ea7a
Kim Do Ky <makingfunk0@gmail.com>
2018-03-29 21:20:27 +0900

    cbv / add post_del

--- daily/cbv/blog/views.py
+++ daily/cbv/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -10,3 +11,5 @@ post_detail = DetailView.as_view(model=Post)
 post_new = CreateView.as_view(model=Post, fields='__all__')
 
 post_edit = UpdateView.as_view(model=Post, fields='__all__')
+
+post_del = DeleteView.as_view(model=Post, success_url=reverse_lazy('blog:post_list'))
