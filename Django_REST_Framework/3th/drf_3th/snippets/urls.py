d022075da296b061afd99bb5d852a98c85ff926a
Kim Do Ky <makingfunk0@gmail.com>
2018-04-03 22:22:41 +0900

    drf / authenticated / add owner in model, add endpoint, conect code snippet, update serializer, adding request permissions to a view, adding a login to a searchable api, setting permissions at the object level, attaching a certificate to an api

--- Django_REST_Framework/3th/drf_3th/snippets/urls.py
+++ Django_REST_Framework/3th/drf_3th/snippets/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
+from django.conf.urls import include
+
 
 urlpatterns = [
+    path('users/', views.UserList.as_view()),
+    path('users/<pk>/', views.UserDetail.as_view()),
     path('snippets/', views.SnippetList.as_view()),
     path('snipptes/<pk>/', views.SnippetDetail.as_view()),
     ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+    ]
