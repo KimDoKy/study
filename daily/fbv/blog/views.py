05aedf89c496f4c088bca513dd6306957ef4b6c1
Kim Do Ky <makingfunk0@gmail.com>
2018-04-05 19:52:14 +0900

    daily / fbv / add post_del

--- daily/fbv/blog/views.py
+++ daily/fbv/blog/views.py
@@ -35,3 +35,9 @@ def post_edit(request, pk):
         forms = PostForm(instance=qs)
     return render(request, 'blog/post_form.html', {'forms':forms})
 
+def post_del(request, pk):
+    qs = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        qs.delete()
+        return redirect('blog:post_list')
+    return render(request, 'blog/post_confirm_delete.html', {'post':qs})
