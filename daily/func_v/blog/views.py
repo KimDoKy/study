ddf5502e4dc55c31fcf37d85fdbdd52a3a3b2128
Kim Do Ky <makingfunk0@gmail.com>
2018-04-29 16:46:24 +0900

    daily / fbv / add post_new

--- daily/func_v/blog/views.py
+++ daily/func_v/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
@@ -13,3 +14,14 @@ def post_list(request):
 def post_detail(request, pk):
     qs = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post':qs})
+
+ 
+def post_new(request):
+    if request.method == 'POST':
+        forms = PostForm(request.POST, request.FILES)
+        if forms.is_valid():
+            post = forms.save()
+            return redirect(post)
+    else:
+        forms = PostForm()
+    return render(request, 'blog/post_form.html', {'forms':forms})
