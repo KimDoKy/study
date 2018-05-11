fdac85bcd52a8318ceb067a1d7c04062cf57c788
Kim Do Ky <makingfunk0@gmail.com>
2018-05-12 01:26:26 +0900

    daily / add use_2 field in coupon models

--- daily/login_practice/coupon/models.py
+++ daily/login_practice/coupon/models.py
@@ -5,8 +5,10 @@ from django.conf import settings
 class Coupon(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=7)
-    use_date = models.DateTimeField(auto_now=True, blank=True)
-    use_date_2nd = models.DateTimeField(auto_now=True, blank=True)
+    use_1 = models.BooleanField()
+    use_date = models.DateTimeField(auto_now=False, blank=True)
+    use_2 = models.BooleanField()
+    use_2_date = models.DateTimeField(auto_now=False, blank=True)
     image = models.ImageField()
 
     def __str__(self):
