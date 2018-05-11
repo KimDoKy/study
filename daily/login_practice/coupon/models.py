5d4ca035a1cdcfeacdc3d021e4adf9712c4bc9f8
Kim Do Ky <makingfunk0@gmail.com>
2018-05-11 20:44:47 +0900

    daily / login_pr / migrate coupon model

--- daily/login_practice/coupon/models.py
+++ daily/login_practice/coupon/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
 
-# Create your models here.
+class Coupon(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=7)
+    use_date = models.DateTimeField(auto_now=True, blank=True)
+    use_date_2nd = models.DateTimeField(auto_now=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('coupon:coupon_list')
