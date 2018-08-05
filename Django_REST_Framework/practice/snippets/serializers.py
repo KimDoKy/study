1003d585ac11d59c6cff088cc63ec019f6d0464d
Kim Do Ky <makingfunk0@gmail.com>
2018-08-05 14:46:16 +0900

    drf / 사용자 모델에 엔드포인트 추가

--- Django_REST_Framework/practice/snippets/serializers.py
+++ Django_REST_Framework/practice/snippets/serializers.py
@@ -1,4 +1,5 @@
 from django.forms import widgets
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
@@ -8,3 +9,9 @@ class SnippetSerializer(serializers.ModelSerializer):
         model = Snippet
         fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
 
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    
+    class Meta:
+        model = User
+        fields = ('id', 'username' ,'snippets')
