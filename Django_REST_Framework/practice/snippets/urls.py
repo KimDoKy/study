b53ea3f993438669873e75d801549b1061941df2
Kim Do Ky <makingfunk0@gmail.com>
2018-08-05 15:11:05 +0900

    drf / snippet 의 하이라이트 버전에 대한 엔드포인트 만들기

--- Django_REST_Framework/practice/snippets/urls.py
+++ Django_REST_Framework/practice/snippets/urls.py
@@ -3,8 +3,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
+    path('', views.api_root),
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<pk>/', views.SnippetDetail.as_view()),
+    path('snippets/<pk>/highlight/', views.SnippetHighlight.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<pk>/', views.UserDetail.as_view()),
 ]
