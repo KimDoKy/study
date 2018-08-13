7102f44936af2d551aa86600b9dbc8725a8e9ed2
Kim Do Ky <makingfunk0@gmail.com>
2018-08-13 17:25:43 +0900

    daily / add simple rest api

--- daily/prac_2/blog/views.py
+++ daily/prac_2/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from blog.serializers import PostSerializer, UserSerializer
+from .models import Post
 
-# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
