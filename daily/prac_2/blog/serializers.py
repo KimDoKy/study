65687d93b8f7d2ddeec282f9bcf771aad3425b63
Kim Do Ky <makingfunk0@gmail.com>
2018-08-13 20:21:59 +0900

    daily / modifed username in serializer, serializers.RelatedField

--- daily/prac_2/blog/serializers.py
+++ daily/prac_2/blog/serializers.py
@@ -2,7 +2,13 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from blog.models import Post
 
+class UserNameField(serializers.RelatedField):
+    def to_representation(self, value):
+        user_name = value.username
+        return '%s' % user_name
+
 class PostSerializer(serializers.HyperlinkedModelSerializer):
+    author = UserNameField(read_only=True)
     class Meta:
         model = Post
         fields = ('title', 'content', 'photo', 'author')
