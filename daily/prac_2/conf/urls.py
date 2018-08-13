b4bedc9c91eedddbf813497aacfe5fda35310739
Kim Do Ky <makingfunk0@gmail.com>
2018-08-13 18:33:34 +0900

    daily / settings static

--- daily/prac_2/conf/urls.py
+++ daily/prac_2/conf/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from blog import views
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'posts', views.PostViewSet)
@@ -12,3 +14,5 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('blog/', include('blog.urls', namespace='blog')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
