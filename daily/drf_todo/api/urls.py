ce9c15b4d3880b37aa43389755ecb41d7b598e85
Kim Do Ky <makingfunk0@gmail.com>
2018-08-13 21:13:25 +0900

    user authentication

--- daily/drf_todo/api/urls.py
+++ daily/drf_todo/api/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from .views import TodoViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('', TodoViewSet, base_name='todos')
 urlpatterns = router.urls
+urlpatterns += [
+    path('rest-auth/', include('rest_auth.urls')),
+    ]
