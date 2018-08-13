5a6bfb4e79e647c5534ce91412f0cc9c7b5abcd1
Kim Do Ky <makingfunk0@gmail.com>
2018-08-13 18:27:57 +0900

    daily / add post_new

--- daily/prac_2/blog/views.py
+++ daily/prac_2/blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from blog.serializers import PostSerializer, UserSerializer
 from .models import Post
+from .forms import PostForm
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
@@ -22,3 +23,13 @@ def post_list(request):
 def post_detail(request, pk):
     q = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post':q})
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form':form})
