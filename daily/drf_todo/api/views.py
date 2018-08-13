39fb0e5d7ed131aa5e0427edec367bb8bf882020
Kim Do Ky <makingfunk0@gmail.com>
2018-08-13 21:03:30 +0900

    generics -> viewset

--- daily/drf_todo/api/views.py
+++ daily/drf_todo/api/views.py
@@ -1,11 +1,7 @@
-from rest_framework import generics
+from rest_framework import viewsets
 from todos import models
 from . import serializers
 
-class ListTodo(generics.ListCreateAPIView):
-    queryset = models.Todo.objects.all()
-    serializer_class = serializers.TodoSerializer
-
-class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
+class TodoViewSet(viewsets.ModelViewSet):
     queryset = models.Todo.objects.all()
     serializer_class = serializers.TodoSerializer
