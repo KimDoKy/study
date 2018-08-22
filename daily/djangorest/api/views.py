359974b365f6c970ff92735234ad3c14d9cfcf94
Kim Do Ky <makingfunk0@gmail.com>
2018-08-23 00:22:36 +0900

    TDA / custom permission

--- daily/djangorest/api/views.py
+++ daily/djangorest/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from .permissions import IsOwner
 from .serializers import BucketlistSerializer
 from .models import Bucketlist
 
@@ -6,7 +7,7 @@ from .models import Bucketlist
 class CreateView(generics.ListCreateAPIView):
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -15,3 +16,6 @@ class CreateView(generics.ListCreateAPIView):
 class DetailsView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
+    permission_classes = (
+            permissions.IsAuthenticated,
+            IsOwner)
