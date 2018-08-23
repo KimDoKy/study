f23d0376d81b93fb8955b74e4b89a98b8f52f288
Kim Do Ky <makingfunk0@gmail.com>
2018-08-23 13:13:27 +0900

    TDA / 사용자가 토큰을 얻기 위해 URL을 제공

--- daily/djangorest/api/urls.py
+++ daily/djangorest/api/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateView, DetailsView
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import CreateView, DetailsView, UserView, UserDetailsView
 
 urlpatterns = {
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('bucketlists/', CreateView.as_view(), name='create'),
     path('bucketlists/<pk>/', DetailsView.as_view(), name='details'),
+    path('users/', UserView.as_view(), name='users'),
+    path('users/<pk>/', UserDetailsView.as_view(), name='user_detail'),
+    path('get-token/', obtain_auth_token),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
