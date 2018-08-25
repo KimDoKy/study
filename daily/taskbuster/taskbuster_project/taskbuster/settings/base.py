3f17351a8815da1032c1cacaec12889af144c3ac
Kim Do Ky <makingfunk0@gmail.com>
2018-08-25 18:58:37 +0900

    TB / secret 분리

--- daily/taskbuster/taskbuster_project/taskbuster/settings/base.py
+++ daily/taskbuster/taskbuster_project/taskbuster/settings/base.py
@@ -19,8 +19,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_3%qmj@*q+7o4rmbuk%*!&z&sqcmfs29m1+vxe)(cz0_b!&j7f'
+from django.core.exceptions import ImproperlyConfigured
+
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 ALLOWED_HOSTS = []
 
