4041496c5a4e727fe9aec4c4776293df043dccb9
Kim Do Ky <makingfunk0@gmail.com>
2018-08-29 23:37:13 +0900

    TB / testing robots.txt, humans.txt

--- daily/taskbuster/taskbuster_project/taskbuster/urls.py
+++ daily/taskbuster/taskbuster_project/taskbuster/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from django.conf.urls import url
+from .views import home, home_files
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('<filename>', home_files, name='home-files'),
+#  url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
+#            home_files, name='home-files'),
 ]
