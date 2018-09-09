334b7c456bd9e81869345de10d701be90167efdf
Kim Do Ky <makingfunk0@gmail.com>
2018-09-09 16:13:17 +0900

    TB / define, admin Tags model

--- daily/taskbuster/taskbuster_project/taskbuster/apps/taskmanager/models.py
+++ daily/taskbuster/taskbuster_project/taskbuster/apps/taskmanager/models.py
@@ -70,3 +70,26 @@ class Project(models.Model):
 
     def __str__(self):
         return '%s - %s' % (self.user, self.name)
+
+
+class Tag(models.Model):
+    user = models.ForeignKey(
+            Profile,
+            related_name='tags',
+            verbose_name=_('user'),
+            on_delete=models.CASCADE
+            )
+    name = models.CharField(
+            max_length=100,
+            verbose_name=_('Name')
+            )
+    objects = managers.TagManager()
+
+    class Meta:
+        verbose_name = _('Tag')
+        verbose_name_plural = _('Tags')
+        ordering = ('user', 'name')
+        unique_together =  ('user', 'name')
+
+    def __str__(self):
+        return '%s - %s' % (self.user, self.name)
