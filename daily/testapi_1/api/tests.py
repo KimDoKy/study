9ec76c75aabeb794da5621e1fef569bd84f8f9e8
Kim Do Ky <makingfunk0@gmail.com>
2018-09-10 12:53:53 +0900

    testapi / view, serializer

--- daily/testapi_1/api/tests.py
+++ daily/testapi_1/api/tests.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 from .models import Bucketlist
 
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
+
 
 class ModelTestCase(TestCase):
 
@@ -13,3 +17,17 @@ class ModelTestCase(TestCase):
         self.bucketlist.save()
         new_count = Bucketlist.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+
+class ViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.bucketlist_data = {'name': 'Go to Doky'}
+        self.response = self.client.post(
+                reverse('create'),
+                self.bucketlist_data,
+                format='json')
+    
+    def test_api_can_create_a_bucketlist(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
