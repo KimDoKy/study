aab92e5818b0a8c64270ebb770e3006971539612
Kim Do Ky <makingfunk0@gmail.com>
2018-09-13 20:35:42 +0900

    tdd_rest / testing get, put, delete

--- daily/prac_tdd_rest/tdd_drf/api/tests.py
+++ daily/prac_tdd_rest/tdd_drf/api/tests.py
@@ -30,3 +30,27 @@ class ViewTestCase(TestCase):
 
     def test_api_can_create_a_bucketlist(self):
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_can_get_a_bucketlist(self):
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.get(
+                reverse('details', kwargs={'pk':bucketlist.id}),
+                format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, bucketlist)
+
+    def test_api_can_update_a_bucketlist(self):
+        bucketlist = Bucketlist.objects.get()
+        bucketlist_data = {'name':'Doky'}
+        res = self.client.put(
+                reverse('details', kwargs={'pk':bucketlist.id}),
+                bucketlist_data,
+                format='json')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_a_bucketlist(self):
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.delete(
+                reverse('details', kwargs={'pk':bucketlist.id}),
+                format='json', follow=True)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
--- daily/prac_tdd_rest/tdd_drf/api/views.py
+++ daily/prac_tdd_rest/tdd_drf/api/views.py
@@ -10,3 +10,7 @@ class CreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save()
 
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
