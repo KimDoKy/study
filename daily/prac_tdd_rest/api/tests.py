c8abcf7d7bd425a3db5276b26fc5c4731d5df5bb
Kim Do Ky <makingfunk0@gmail.com>
2018-09-14 13:19:57 +0900

    tdd_rest / add CreateView, test

--- daily/prac_tdd_rest/api/tests.py
+++ daily/prac_tdd_rest/api/tests.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
 from .models import Bucketlist
 
 
@@ -13,3 +16,18 @@ class ModelTestCase(TestCase):
         self.bucketlist.save()
         new_count = Bucketlist.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+        
+class ViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.bucketlist_data = {'name':'Doky'}
+        self.response = self.client.post(
+                reverse('create'),
+                self.bucketlist_data,
+                format='json')
+
+    def test_api_can_create_a_bucketlist(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
