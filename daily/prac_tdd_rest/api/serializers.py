26a152d0477c654ca211898e8599912feab910e8
Kim Do Ky <makingfunk0@gmail.com>
2018-09-18 12:48:16 +0900

    tdd_rest / reset

--- daily/prac_tdd_rest/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Bucket
-
-class BucketSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Bucket
-        fields = ('id', 'name', 'date_created', 'date_modified')
-        read_only_fields = ('date_created', 'date_modified')
--- daily/prac_tdd_rest/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CreateView, DetailView
-
-urlpatterns = [
-    path('bucketlists/', CreateView.as_view(), name='create'),
-    path('bucketlists/<pk>/', DetailView.as_view(), name='detail'),
-    ]
