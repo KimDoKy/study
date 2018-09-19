e988373ac0ec8130467a3fb4f4cbe904afc4623e
Kim Do Ky <makingfunk0@gmail.com>
2018-09-19 12:58:07 +0900

    tdd_rest / add test - get, put, delete

--- daily/prac_tdd_rest/api/tests.py
+++ daily/prac_tdd_rest/api/tests.py
@@ -30,3 +30,25 @@ class ViewTestCase(TestCase):
 
     def test_api_can_create_a_bucket(self):
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_can_get_a_bucket(self):
+        bucket = Bucket.objects.get()
+        response = self.client.get(
+                '/bucketlists/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_api_can_update(self):
+        bucket = Bucket.objects.get()
+        change_data = {'name':'change test'}
+        res = self.client.put(
+                reverse('detail', kwargs={'pk':bucket.id}),
+                change_data,
+                format='json')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_a_bucket(self):
+        bucket = Bucket.objects.get()
+        response = self.client.delete(
+                reverse('detail', kwargs={'pk':bucket.id}),
+                format='json', follow=True)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
--- daily/prac_tdd_rest/api/views.py
+++ daily/prac_tdd_rest/api/views.py
@@ -6,3 +6,8 @@ from .serializers import BucketSerializer
 class CreateView(generics.ListCreateAPIView):
     queryset = Bucket.objects.all()
     serializer_class = BucketSerializer
+
+
+class DetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bucket.objects.all()
+    serializer_class =BucketSerializer
