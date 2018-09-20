4e7d6e8f030037ff3cae89d15cfa4837045611e9
Kim Do Ky <makingfunk0@gmail.com>
2018-09-20 13:09:48 +0900

    tdd_rest / settings token

--- daily/prac_tdd_rest/api/models.py
+++ daily/prac_tdd_rest/api/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
 class Bucket(models.Model):
@@ -9,3 +13,8 @@ class Bucket(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
