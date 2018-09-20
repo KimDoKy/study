5d6b76e21b6079ae726a50a41b20161f5fd1b55d
Kim Do Ky <makingfunk0@gmail.com>
2018-09-20 12:57:34 +0900

    tdd_rest / add permission

--- daily/prac_tdd_rest/api/views.py
+++ daily/prac_tdd_rest/api/views.py
@@ -1,11 +1,13 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Bucket
 from .serializers import BucketSerializer
+from .permissions import IsOwner
 
 
 class CreateView(generics.ListCreateAPIView):
     queryset = Bucket.objects.all()
     serializer_class = BucketSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -13,3 +15,4 @@ class CreateView(generics.ListCreateAPIView):
 class DetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Bucket.objects.all()
     serializer_class = BucketSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
