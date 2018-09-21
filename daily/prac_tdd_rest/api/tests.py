3f045ff4595e38a3588e9bc6f8e7e35e1c80e3cf
Kim Do Ky <makingfunk0@gmail.com>
2018-09-21 13:13:10 +0900

    tdd_rest / define CreateView, write test

--- daily/prac_tdd_rest/api/tests.py
+++ daily/prac_tdd_rest/api/tests.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
 from .models import Todo
 
 
@@ -8,8 +11,23 @@ class ModelTestCase(TestCase):
         self.name = 'write code'
         self.todo = Todo(name=self.name)
 
-    def test_model_can_a_todo(self):
+    def test_model_can_create_a_todo(self):
         old_count = Todo.objects.count()
         self.todo.save()
         new_count = Todo.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+
+class ViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.todo_data = {'name':'view test'}
+        self.response = self.client.post(
+                reverse('create'),
+                self.todo_data,
+                format='json')
+
+    def test_api_can_create_a_todo(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
--- daily/prac_tdd_rest/api/views.py
+++ daily/prac_tdd_rest/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import TodoSerializer
+from .models import Todo
 
-# Create your views here.
+
+class CreateView(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
