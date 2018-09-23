42550d8afcbb30168099c51d0dcb89fa5b0726da
Kim Do Ky <makingfunk0@gmail.com>
2018-09-24 02:15:55 +0900

    daily / write test get, put, delete

--- daily/blog_prac/blog/tests.py
+++ daily/blog_prac/blog/tests.py
@@ -32,3 +32,27 @@ class ViewTestCase(TestCase):
 
     def test_blog_can_create_a_post(self):
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_blog_can_get_a_post(self):
+        post = Post.objects.get()
+        response = self.client.get(
+                reverse('detail', kwargs={'pk':post.id}),
+                format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, post)
+
+    def test_blog_can_update_a_post(self):
+        post = Post.objects.get()
+        change_data = {'title':'view test title', 'content':'view test content'}
+        res = self.client.put(
+                reverse('detail', kwargs={'pk':post.id}),
+                change_data,
+                format='json')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_blog_can_delete_a_post(self):
+        post = Post.objects.get()
+        response = self.client.delete(
+                reverse('detail', kwargs={'pk':post.id}),
+                format='json', follow=True)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
--- daily/blog_prac/blog/views.py
+++ daily/blog_prac/blog/views.py
@@ -10,3 +10,7 @@ class CreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+class DetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
