56d0530a718fe8e9e2b88492b3798ebba9811cf6
Kim Do Ky <makingfunk0@gmail.com>
2018-09-23 16:21:58 +0900

    tdd_res / add token authentication

--- daily/prac_tdd_rest/tdd/urls.py
+++ daily/prac_tdd_rest/tdd/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todolist/', include('api.urls')),
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('get-token/', obtain_auth_token),
 ]
