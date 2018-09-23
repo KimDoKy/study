3a55c19698ea80957288aefad1ebfbe147da335b
Kim Do Ky <makingfunk0@gmail.com>
2018-09-24 00:58:08 +0900

    tdd_rest / add token authentication

--- daily/prac_tdd_rest/api/models.py
+++ daily/prac_tdd_rest/api/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
 class Post(models.Model):
@@ -9,3 +12,7 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.title}'
+
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
