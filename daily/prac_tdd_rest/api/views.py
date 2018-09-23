12c50bb8409e724d134c102e0c1b56877f029ba6
Kim Do Ky <makingfunk0@gmail.com>
2018-09-23 14:52:44 +0900

    tdd_rest / add views - perform_create

--- daily/prac_tdd_rest/api/views.py
+++ daily/prac_tdd_rest/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .serializers import TodoSerializer
 from .models import Todo
 
@@ -6,6 +6,7 @@ from .models import Todo
 class CreateView(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
