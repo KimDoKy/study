a142c9791f335dd584bb341a9558823cc71c5020
Kim Do Ky <makingfunk0@gmail.com>
2018-09-23 15:02:38 +0900

    tdd_rest / restartproject tdd, migrate Todo model, create api app

---
+++ daily/prac_tdd_rest/api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=20, blank=False, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title}'
