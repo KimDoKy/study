54cf2aaf8b2b1021e6f17056f66ddf91ef7e861f
Kim Do Ky <makingfunk0@gmail.com>
2018-09-24 19:15:49 +0900

    daily / add post_edit

--- daily/blog_prac/blog/urls.py
+++ daily/blog_prac/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreateView, DetailView, post_list, post_detail, post_new
+from .views import CreateView, DetailView, post_list, post_detail, post_new, post_edit
 
 urlpatterns = [
     path('api/', CreateView.as_view(), name='create'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('new/', post_new, name='post_new'),
     path('', post_list, name='post_list'),
     path('<pk>/', post_detail, name='post_detail'),
+    path('<pk>/edit/', post_edit, name='post_edit'),
     ]
