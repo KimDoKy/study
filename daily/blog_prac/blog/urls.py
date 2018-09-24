510e157bd686f550606095445dd87703543fc0e7
Kim Do Ky <makingfunk0@gmail.com>
2018-09-24 18:22:28 +0900

    daily / add post_detail

--- daily/blog_prac/blog/urls.py
+++ daily/blog_prac/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreateView, DetailView, post_list
+from .views import CreateView, DetailView, post_list, post_detail
 
 urlpatterns = [
     path('api/', CreateView.as_view(), name='create'),
     path('api/<pk>/', DetailView.as_view(), name='detail'),
     path('', post_list, name='post_list'),
+    path('<pk>/', post_detail, name='post_detail'),
     ]
--- daily/blog_prac/blog/views.py
+++ daily/blog_prac/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
@@ -18,3 +18,7 @@ class DetailView(generics.RetrieveUpdateDestroyAPIView):
 def post_list(request):
     post = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts':post})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
