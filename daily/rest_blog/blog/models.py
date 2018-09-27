762c74e353e56ec6055f3bf3a7fa7be1737b3431
KimDoKy <makingfunk0@gmail.com>
2018-09-27 10:27:24 +0900

    rest_blog / migrate Post model and write test

--- daily/rest_blog/blog/models.py
+++ daily/rest_blog/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=30, blank=False)
+    content = models.TextField(blank=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    photo = models.ImageField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title}'
