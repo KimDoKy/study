7771a3e977297202ccb6876f2a518d4801f3097f
KimDoKy <makingfunk0@gmail.com>
2018-09-28 09:32:42 +0900

    rest_blog / add post_list, write test

--- daily/rest_blog/blog/tests.py
+++ daily/rest_blog/blog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
 
-# Create your tests here.
+class BlogTestCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_blog_can_get_post(self):
+        response = self.client.get(
+                '', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
