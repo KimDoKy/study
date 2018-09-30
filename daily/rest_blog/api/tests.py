185f073e4d106e815ba91756801f179c1138cdcd
KimDoKy <makingfunk0@gmail.com>
2018-10-01 00:59:30 +0900

    rest_blog / add CreateView, write test

--- daily/rest_blog/api/tests.py
+++ daily/rest_blog/api/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
 from blog.models import Post
 
 class ModelTestCase(TestCase):
@@ -15,3 +18,18 @@ class ModelTestCase(TestCase):
         new_count = Post.objects.count()
         self.assertNotEqual(old_count, new_count)
         
+
+class ViewTestCase(TestCase):
+
+    def setUp(self):
+        user = User.objects.create(username='view_tester')
+        self.client = APIClient()
+        self.client.force_authenticate(user=user)
+        self.post_data = {'title':'view test title', 'author':user.id}
+        self.response = self.client.post(
+            reverse('create'),
+            self.post_data,
+            format='json')
+
+    def test_api_can_create_a_post(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
