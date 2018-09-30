c4a63a21c49ff5bde4946738a1004709e6e6dcd9
KimDoKy <makingfunk0@gmail.com>
2018-09-30 16:37:00 +0900

    rest_blog / reset

--- daily/rest_blog/blog/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-from .models import Post
-
-class IsAuthor(BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        if isinstance(obj, Post):
-            return obj.author == request.user
-        return obj.author == request.user
