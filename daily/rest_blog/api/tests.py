aa1329020ac962e518e56f8286c3283d7aa2455e
KimDoKy <makingfunk0@gmail.com>
2018-10-01 00:43:29 +0900

    rest_blog / migrate post model, write api test

--- daily/rest_blog/api/tests.py
+++ daily/rest_blog/api/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from blog.models import Post
 
-# Create your tests here.
+class ModelTestCase(TestCase):
+
+    def setUp(self):
+        user = User.objects.create(username='tester')
+        self.title = 'model test title'
+        self.post = Post(title=self.title, author=user)
+
+    def test_model_can_create_a_post(self):
+        old_count = Post.objects.count()
+        self.post.save()
+        new_count = Post.objects.count()
+        self.assertNotEqual(old_count, new_count)
+        
--- daily/rest_blog/blog/models.py
+++ daily/rest_blog/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30, blank=False)
+    author = models.ForeignKey('auth.User', related_name='owner', on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title}'
