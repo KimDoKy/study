c950153624428bcfb4ec93c29085b472ff1f7e9a
KimDoKy <makingfunk0@gmail.com>
2018-10-01 12:59:28 +0900

    rest_blog / add permission

--- daily/rest_blog/api/views.py
+++ daily/rest_blog/api/views.py
@@ -1,10 +1,13 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from blog.models import Post
 from blog.serializers import PostSerializer
+from blog.permissions import IsAuthor
 
 class CreateView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticated,
+                          IsAuthor)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -13,3 +16,5 @@ class CreateView(generics.ListCreateAPIView):
 class DetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticated,
+                          IsAuthor)
