21645ab3f092f2aa071449c055bdfaab7dd7881e
KimDoKy <makingfunk0@gmail.com>
2018-10-02 23:30:14 +0900

    rest_blog / settings static

--- daily/rest_blog/conf/urls.py
+++ daily/rest_blog/conf/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
@@ -8,3 +10,5 @@ urlpatterns = [
     path('get-token/', obtain_auth_token),
     path('blog/', include('blog.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
