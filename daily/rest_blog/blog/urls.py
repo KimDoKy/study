220d741883a0b603cada32d922852aebee18bdec
KimDoKy <makingfunk0@gmail.com>
2018-10-04 00:27:07 +0900

    rest_blog / add post_delete

--- daily/rest_blog/blog/urls.py
+++ daily/rest_blog/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import post_list, post_detail, post_new, post_edit
+from .views import post_list, post_detail, post_new, post_edit, post_del
 
 urlpatterns = [
     path('', post_list, name='post_list'),
     path('new/', post_new, name='post_new'),
     path('<pk>/', post_detail, name='post_detail'),
     path('<pk>/edit/', post_edit, name='post_edit'),
+    path('<pk>/del/', post_del, name='post_del'),
 ]
