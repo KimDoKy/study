7ded358d1df3ea4e6230d156e9e52a4d92856847
KimDoKy <makingfunk0@gmail.com>
2018-10-07 20:17:14 +0900

    prac_tdd / add CreateView, write test

--- daily/prac_tdd/api/views.py
+++ daily/prac_tdd/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from blog.models import Post
+from blog.serializers import PostSerializer
 
-# Create your views here.
+class CreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
