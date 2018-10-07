e0740f6438827f61e7dcc139f71300860bd9bb20
KimDoKy <makingfunk0@gmail.com>
2018-10-07 22:42:55 +0900

    prac_tdd / add token authentication

--- daily/prac_tdd/blog/models.py
+++ daily/prac_tdd/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from rest_framework.authtoken.models import Token
 
 class Post(models.Model):
     title = models.CharField(max_length=20)
@@ -10,3 +11,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def create_auth_token(sender, instance=None, created=False, **kwargs):
+        if created:
+            Token.objects.create(author=instance.user)
