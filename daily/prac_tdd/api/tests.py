7fbfb73c8135443aa77e7cd4b2acf24bbbd765ea
KimDoKy <makingfunk0@gmail.com>
2018-10-07 20:37:23 +0900

    prac_tdd / write test / delete

--- daily/prac_tdd/api/tests.py
+++ daily/prac_tdd/api/tests.py
@@ -50,3 +50,10 @@ class ViewTestCase(TestCase):
             reverse('detail', kwargs={'pk':post.id}),
             update_data, format='json')
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_a_post(self):
+        post = Post.objects.get()
+        response = self.client.delete(
+            reverse('detail', kwargs={'pk':post.id}),
+            format='json', follow=True)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
