faa0fcad0fecdec3e3fa26d03c6742466b40cb9d
KimDoKy <makingfunk0@gmail.com>
2018-10-09 16:03:17 +0900

    pydjango / registe admin, settings url, migrate Bookmark model, add bookmark views

--- daily/pydjango_2.0/pydjango/conf/urls.py
+++ daily/pydjango_2.0/pydjango/conf/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import ListView, DetailView
+from bookmark.models import Bookmark
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # CBV
+    path('bookmark/', ListView.as_view(model=Bookmark), name='index'),
+    path('bookmark/<pk>/', DetailView.as_view(model=Bookmark), name='detail'),
 ]
