61255df108ca430e004fd8a31bc1b447da882933
Kim Do Ky <makingfunk0@gmail.com>
2018-11-13 23:19:03 +0900

    reset customuser

--- daily/customuser/accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class CustomUser(AbstractUser):
-    team = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.team
-
