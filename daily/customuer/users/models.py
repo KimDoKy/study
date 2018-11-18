9a82e8992a225a737a7bf2638ef40aaa92fd1934
Kim Do Ky <makingfunk0@gmail.com>
2018-11-18 22:46:12 +0900

    customuser / migrate user

--- daily/customuer/users/models.py
+++ daily/customuer/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    nick_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nick_name
