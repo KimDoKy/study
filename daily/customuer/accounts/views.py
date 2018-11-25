dc00a19bc347f754206d4353b096f6c2156e8640
Kim Do Ky <makingfunk0@gmail.com>
2018-11-25 15:04:24 +0900

    customuser / write login, signup

--- daily/customuer/accounts/views.py
+++ daily/customuer/accounts/views.py
@@ -1,3 +1,15 @@
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+
+class SignUp(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
+
+def home(request):
+    user = get_user_model()
+    return render(request, 'home.html', {'user':user})
