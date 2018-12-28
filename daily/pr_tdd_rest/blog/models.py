0c932d3350389b3f19d2309b2577f19cbc3363b7
Kim Do Ky <makingfunk0@gmail.com>
2018-12-28 13:28:00 +0900

    rest_tdd / migrate blog model, update test

--- daily/pr_tdd_rest/blog/models.py
+++ daily/pr_tdd_rest/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255, blank=True, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    photo = models.ImageField(upload_to='')
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
--- daily/pr_tdd_rest/blog/tests.py
+++ daily/pr_tdd_rest/blog/tests.py
@@ -1,14 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 from .models import Post
 
 class ModelTestCase(TestCase):
 
     def setUp(self):
-        self.post_name = "Write test code"
-        self.post = Post(name=self.post_name)
+        self.author = User.objects.create(username='tester')
+        self.author.save()
+        self.post_title = "Write test code"
+        self.post = Post(author=self.author, title=self.post_title)
 
     def test_model_can_create_a_post(self):
         old_count = Post.objects.count()
         self.post.save()
-        new_count = Post.object.count()
-        self.assertEqual(old_count, new_count)
+        new_count = Post.objects.count()
+        self.assertNotEqual(old_count, new_count)
