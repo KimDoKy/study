1f6d585e3e1ce16711864a0914f37a3ae15fcbaf
Kim Do Ky <makingfunk0@gmail.com>
2018-12-30 01:39:07 +0900

    rest_tdd / add permission

--- daily/pr_tdd_rest/blog/urls.py
+++ daily/pr_tdd_rest/blog/urls.py
@@ -5,6 +5,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import CreateView, DetailView
 
 urlpatterns = {
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('posts/', CreateView.as_view(), name='create'),
     path('posts/<pk>/', DetailView.as_view(), name='details'),
 }
--- daily/pr_tdd_rest/blog/views.py
+++ daily/pr_tdd_rest/blog/views.py
@@ -1,16 +1,21 @@
 from rest_framework import generics
+from rest_framework import permissions
 
 from .serializers import PostSerializer
+from .permissions import IsAuthor
 from .models import Post
 
 from django.contrib.auth.models import User
+
 class CreateView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticated, IsAuthor)
 
     def perform_create(self, serializer):
-        serializer.save()
+        serializer.save(author=self.request.user)
 
 class DetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticated, IsAuthor)
