89b2de1611344b1e733fe6fe38b0b70f98a781da
Kim Do Ky <makingfunk0@gmail.com>
2018-12-30 20:39:03 +0900

    tdd_rest_blog / migrate Post model, write Model test case

--- daily/tdd_rest_blog/api/tests.py
+++ daily/tdd_rest_blog/api/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from blog.models import Post
+
+class ModelTestCase(TestCase):
+
+    def setUp(self):
+        user = User(username='tester')
+        user.save()
+        self.post_title = "Test Title"
+        self.post = Post(title=self.post_title, author=user)
+
+    def test_model_can_create_a_post(self):
+        old_count = Post.objects.count()
+        self.post.save()
+        new_count = Post.objects.count()
+        self.assertNotEqual(old_count, new_count)
