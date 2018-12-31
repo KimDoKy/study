71daf8fc9633b3c1f4254739a648aac7bf0fa928
Kim Do Ky <makingfunk0@gmail.com>
2018-12-31 17:07:55 +0900

    tdd_rest_blog / login url

--- daily/tdd_rest_blog/api/urls.py
+++ daily/tdd_rest_blog/api/urls.py
@@ -1,11 +1,12 @@
 # api urls
-from django.urls import path
+from django.urls import path, include
 
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from .views import CreateView, DetailView
 
 urlpatterns = [
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('posts/', CreateView.as_view(), name='create'),
     path('posts/<pk>/', DetailView.as_view(), name='details'),
     ]
