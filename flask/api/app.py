8f18b67ec94a281fe6141c02679e0e40b29e8df1
Kim Do Ky <makingfunk0@gmail.com>
2019-05-22 13:03:29 +0900

    flask / add sign up

--- flask/api/app.py
+++ flask/api/app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+app.users = {}
+app.id_count = 1
 
 @app.route("/ping", methods=['GET'])
 def ping():
     return "pong"
+
+@app.route("/sign-up", methods=['POST'])
+def sign_up():
+    new_user = request.json
+    new_user["id"] = app.id_count
+    app.users[app.id_count] = new_user
+    app.id_count
+
+    return jsonify(new_user)
