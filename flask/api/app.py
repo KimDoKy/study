60909143151c88b9e871f900d74ec14d3c943bc8
Kim Do Ky <makingfunk0@gmail.com>
2019-05-22 13:13:16 +0900

    flask / tweet 300 limit

--- flask/api/app.py
+++ flask/api/app.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 app.users = {}
 app.id_count = 1
+app.tweets = []
 
 @app.route("/ping", methods=['GET'])
 def ping():
@@ -16,3 +17,22 @@ def sign_up():
     app.id_count
 
     return jsonify(new_user)
+
+@app.route("/tweet", methods=['POST'])
+def tweet():
+    payload = request.json
+    user_id = int(payload['id'])
+    tweet = payload['tweet']
+
+    if user_id not in app.users:
+        return '사용자가 존재하지 않습니다.', 400
+
+    if len(tweet) > 300:
+        return '300자를 초과했습니다.', 400
+
+    app.tweets.append({
+            'user_id': user_id,
+            'tweet': tweet
+            })
+
+    return '', 200
