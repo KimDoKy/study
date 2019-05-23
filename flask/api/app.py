9d310ebc2447fef69c003f2b7b66d3e8347c7983
Kim Do Ky <makingfunk0@gmail.com>
2019-05-23 17:29:06 +0900

    flask / follow, unfollow

--- flask/api/app.py
+++ flask/api/app.py
@@ -1,9 +1,18 @@
 from flask import Flask, jsonify, request
+from flask.json import JSONEncoder
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+
+        return CustomJSONEncoder.default(self, obj)
 
 app = Flask(__name__)
 app.users = {}
 app.id_count = 1
 app.tweets = []
+app.json_encoder = CustomJSONEncoder
 
 @app.route("/ping", methods=['GET'])
 def ping():
@@ -14,7 +23,7 @@ def sign_up():
     new_user = request.json
     new_user["id"] = app.id_count
     app.users[app.id_count] = new_user
-    app.id_count
+    app.id_count = app.id_count + 1
 
     return jsonify(new_user)
 
@@ -36,3 +45,31 @@ def tweet():
             })
 
     return '', 200
+
+@app.route("/follow", methods=['POST'])
+def follow():
+    payload = request.json
+    user_id = int(payload['id'])
+    user_id_to_follow = int(payload['follow'])
+
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return '사용자가 존재하지 않습니다.', 400
+
+    user = app.users[user_id]
+    user.setdefault('follow', set()).add(user_id_to_follow)
+
+    return jsonify(user)
+
+@app.route('/unfollow', methods=['POST'])
+def unfollow():
+    payload = request.json
+    user_id = int(payload['id'])
+    user_id_to_follow = int(payload['unfollow'])
+
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return '사용자가 존재하지 않습니다.', 400
+
+    user = app.users[user_id]
+    user.setdefault('follow', set()).discard(user_id_to_follow)
+
+    return jsonify(user)
