daf9d8f3a3084142c37ad938850a2119db23ec3b
Kim Do Ky <makingfunk0@gmail.com>
2019-05-30 21:00:45 +0900

    algorithm / 선택 정렬

--- algorithm/sort_search.py
+++ algorithm/sort_search.py
@@ -9,3 +9,17 @@ def search_list(a, x):
 # v_list = [23,54,75,34,76,87]
 # print(search_list(v_list, 76))
 # print(search_list(v_list, 99))
+
+# 선택 정렬
+def sel_sort(a):
+    n = len(a)
+    for i in range(0, n-1):
+        min_idx = i
+        for j in range(i+1, n):
+            if a[j] < a[min_idx]:
+                min_idx = j
+                a[i], a[min_idx] = a[min_idx], a[i]
+
+d = [2, 5, 6, 4, 7]
+sel_sort(d)
+print(d)
