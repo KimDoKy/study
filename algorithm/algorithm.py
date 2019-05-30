c6b7d828f3d5d62caddf13c17f19930ecd68231d
Kim Do Ky <makingfunk0@gmail.com>
2019-05-30 20:19:02 +0900

    algorithm / binary search

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -13,3 +13,21 @@ def is_palindrome(word):
             triger = False
             break
     return triger
+
+# binary_search
+
+def binary_search(element, some_list):
+    s_idx = 0
+    e_idx = len(some_list) - 1
+    
+    while s_idx <= e_idx:
+        mid_idx = (s_idx + e_idx) // 2
+        if element == some_list[mid_idx]:
+            return mid_idx
+        elif element > some_list[mid_idx]:
+            s_idx = mid_idx + 1
+        elif element < some_list[mid_idx]:
+            e_idx = mid_idx - 1
+        else:
+            return None
+
