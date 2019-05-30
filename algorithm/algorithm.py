341febc32bb70ac1fd668c80645d10ecf1d2529b
Kim Do Ky <makingfunk0@gmail.com>
2019-05-30 20:15:27 +0900

    algorithm / palindrome

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -3,3 +3,13 @@
 def sum(n):
     return n * (n+1) // 2
 
+# palindrome
+
+def is_palindrome(word):
+    for i in range(len(word)):
+        if word[i] == word[(i+1) * -1)]:
+            triger = True
+        else:
+            triger = False
+            break
+    return triger
