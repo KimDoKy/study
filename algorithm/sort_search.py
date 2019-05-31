1603110003ce23c5acaa7f786fa03e24b3274bd9
Kim Do Ky <makingfunk0@gmail.com>
2019-05-31 12:50:57 +0900

    algorithm / 삽입정렬, 병합정렬

--- algorithm/sort_search.py
+++ algorithm/sort_search.py
@@ -20,6 +20,57 @@ def sel_sort(a):
                 min_idx = j
                 a[i], a[min_idx] = a[min_idx], a[i]
 
-d = [2, 5, 6, 4, 7]
-sel_sort(d)
-print(d)
+# d = [2, 5, 6, 4, 7]
+# sel_sort(d)
+# print(d)
+
+# 삽입 정렬
+def ins_sort(a):
+    n = len(a)
+    for i in range(1, n):
+        key = a[i]
+        j = i - 1
+        while j >= 0 and a[j] > key:
+            a[j+1] = a[j]
+            j -= 1
+        a[j+1] = key
+
+# d = [2, 5, 6, 4, 7]
+# ins_sort(d)
+# print(d)
+
+# 병합 정렬
+def merge_sort(a):
+    n = len(a)
+    if n <= 1:
+        return
+    mid = n // 2
+    g1 = a[:mid]
+    g2 = a[mid:]
+    merge_sort(g1)
+    merge_sort(g2)
+    i1 = 0
+    i2 = 0
+    ia = 0
+    while i1 < len(g1) and i2 < len(g2):
+        if g1[i1] < g2[i2]:
+            a[ia] = g1[i1]
+            i1 += 1
+            ia += 1
+        else:
+            a[ia] = g2[i2]
+            i2 += 1
+            ia += 1
+    while i1 < len(g1):
+        a[ia] = g1[i1]
+        i1 += 1
+        ia += 1
+    while i2 < len(g2):
+        a[ia] = g2[i2]
+        i2 += 1
+        ia += 1
+
+# d = [6,8,9,7,20,34,5,2,3]
+# print(d)
+# merge_sort(d)
+# print(d)
