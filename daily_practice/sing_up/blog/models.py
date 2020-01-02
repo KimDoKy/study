3200b10cc553f7a27dc78d4060bda0c0660e29ab
Kim Do Ky <makingfunk0@gmail.com>
2020-01-03 01:12:33 +0900

    포맷 전 전체 업로드

---
+++ daily_practice/sing_up/blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.conf import settings
+from django.urls import reverse
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    photo = models.ImageField(blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    message = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
