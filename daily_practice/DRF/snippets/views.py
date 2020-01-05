c3a548e9afe2556c4b395fde27119bc01a2ffe79
KimDoKy <makingfunk0@gmail.com>
2020-01-05 15:49:17 +0900

    drf / 라우터로 url 정리 / 안쓰는 임포트 삭제

--- daily_practice/DRF/snippets/views.py
+++ daily_practice/DRF/snippets/views.py
@@ -1,22 +1,11 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
 from rest_framework import permissions
-from rest_framework.decorators import api_view, action
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework import renderers
-from rest_framework.reverse import reverse
 from rest_framework import viewsets
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
-
-
-@api_view(('GET',))
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-        })
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
