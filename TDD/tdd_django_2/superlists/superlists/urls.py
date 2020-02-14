962abec816fd43ef18d9f71c746a3a605fd10de3
KimDoKy <makingfunk0@gmail.com>
2020-02-14 17:08:01 +0900

    reset / 버전 차이와 교재의 최신 버전 코드 대량 변경으로 처음부터 다시 진행

--- TDD/tdd_django_2/superlists/superlists/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-import lists.views as lists
-
-urlpatterns = [
-    path('', lists.home_page, name='home'),
-    path('admin/', admin.site.urls),
-    ]
